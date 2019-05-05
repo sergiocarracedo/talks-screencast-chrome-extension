993607c8b1a2b187a28b12a5da52024872b295df
Sergio Carracedo <info@sergiocarracedo.es>
2019-05-05 10:45:04 +0200

    Save to file

--- src/options/ConfigTab.vue
+++ src/options/ConfigTab.vue
@@ -9,7 +9,7 @@
     <v-container grid-list-md>
       <v-layout justify-center>
         <v-flex xs12 sm8 md8>
-          <v-tabs color="primary" dark>
+          <v-tabs color="primary" dark class="mb-5">
             <v-tab ripple>
               Sources and Settings
             </v-tab>
@@ -32,13 +32,49 @@
             </v-tab-item>
           </v-tabs>
 
-          <v-card>
+          <v-card class="mb-5">
+            <v-card-text>
+              <v-switch
+                  v-model="preview"
+                  color="primary"
+                  label="Preview"
+              ></v-switch>
+            </v-card-text>
+
             <video
                 autoplay="true"
                 ref="captureStreamPreview"
                 class="capture-preview"
                 muted
             />
+
+            <v-card-actions>
+              <v-btn
+                  color="primary"
+                  @click="startRecording"
+                  :disabled="recording"
+              >
+                <v-icon>fiber_manual_record</v-icon> Start recording
+              </v-btn>
+
+              <v-btn
+                  color="primary"
+                  @click="stopRecording"
+                  :disabled="!recording"
+              >
+                <v-icon>stop</v-icon> Stop recording
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn
+                  :href="file"
+                  :disabled="!file"
+                  download="meetup.webm"
+              >
+                <v-icon>save</v-icon> Download
+              </v-btn>
+            </v-card-actions>
           </v-card>
 
           <canvas ref="layoutCanvas"></canvas>
@@ -67,9 +103,16 @@
       Talks
     },
     data: () => ({
+      preview: true,
+      recording: false,
       layoutStream: null,
       cameraStream: null,
-      screenCaptureStream: null
+      screenCaptureStream: null,
+      mixer: null,
+      mixedStream: null,
+      mediaRecorder: null,
+      buffer: [],
+      file: null
     }),
     computed: {
       ug: {
@@ -104,14 +147,79 @@
       },
       cameraStream (newVal, oldVal) {
         this.updatePreview()
+      },
+      preview (newVal) {
+        if (newVal) {
+          this.createPreview()
+        } else {
+          this.destroyPreview()
+        }
       }
     },
     mounted () {
-      this.createLayoutStream()
-      this.setCameraStream()
-      this.updatePreview()
+      this.createPreview()
+    },
+    destroyed () {
+      this.destroyPreview()
+      this.mediaRecorder.stop()
     },
     methods: {
+      startRecording () {
+        const options = {
+          mimeType: 'video/webm',
+          video: {
+            width: 1920,
+            height: 1080
+          }
+        }
+
+        this.mediaRecorder = new MediaRecorder(this.mixedStream, options)
+        this.mediaRecorder.onstop = this.onStopRecording
+        this.mediaRecorder.ondataavailable = this.onDataAvailable
+        this.mediaRecorder.start()
+
+        this.recording = true
+      },
+      stopRecording () {
+        this.mediaRecorder.stop()
+        console.log(this.mediaRecorder)
+      },
+      onStopRecording (e) {
+        this.recording = false
+
+        const blob = new Blob(this.buffer, {
+          type: 'video/webm'
+        })
+
+        this.file = URL.createObjectURL(blob)
+      },
+      onDataAvailable (e) {
+        if (e.data) {
+          this.buffer.push(e.data)
+        }
+      },
+      createPreview () {
+        this.createLayoutStream()
+        this.setCameraStream()
+        console.log('createPreview')
+        this.updatePreview()
+      },
+      destroyPreview () {
+        try {
+          this.mixerStream.releaseStreams()
+          this.mixerStream = null
+        } catch (e) {}
+
+        this.layoutStream.getTracks().forEach(track => track.stop())
+
+        try {
+          this.cameraStream.getTracks().forEach(track => track.stop())
+        } catch (e) {}
+
+        try {
+          this.screenCaptureStream.getTracks().forEach(track => track.stop())
+        } catch (e) {}
+      },
       onScreenCaptureStream (stream) {
         this.screenCaptureStream = stream
       },
@@ -119,15 +227,12 @@
         const canvas = this.$refs['layoutCanvas']
 
         this.updateCanvas()
-
         setInterval(this.updateCanvas, 5000)
 
         this.layoutStream = canvas.captureStream()
         this.layoutStream.width = 1920
         this.layoutStream.height = 1080
         this.layoutStream.fullcanvas = 1
-
-        // this.$refs['captureStreamPreview'].srcObject = this.layoutStream
       },
       updateCanvas () {
         const canvas = this.$refs['layoutCanvas']
@@ -170,14 +275,17 @@
           streams.push(this.cameraStream)
         }
 
-        console.log(streams)
         if (streams.length > 0) {
-          const mixer = new MultiStreamsMixer(streams)
+          if (this.mixer === null) {
+            this.mixer = new MultiStreamsMixer(streams)
+            this.mixedStream = this.mixer.getMixedStream()
+            this.$refs['captureStreamPreview'].srcObject = this.mixedStream
+          } else {
+            this.mixer.resetVideoStreams(streams)
+          }
 
-          mixer.frameInterval = 1
-          mixer.startDrawingFrames()
-
-          this.$refs['captureStreamPreview'].srcObject = mixer.getMixedStream()
+          this.mixer.frameInterval = 1
+          this.mixer.startDrawingFrames()
         }
       }
     }
