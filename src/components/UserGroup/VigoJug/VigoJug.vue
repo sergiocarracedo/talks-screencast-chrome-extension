9fbaf688f320e0bc835f153c2aba386e47476bc1
Sergio Carracedo <info@sergiocarracedo.es>
2019-05-06 19:21:23 +0200

    VigoJug & CoruñaJug Layout

---
+++ src/components/UserGroup/VigoJug/VigoJug.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="vigotech">
+    <obs-header
+        :title="meetupTitle"
+        :logo="true"
+    ></obs-header>
+
+    <obs-aside :isLightning="currentTalk.isLightning"></obs-aside>
+
+    <obs-footer :speaker="currentTalk.speaker" :title="currentTalk.title"></obs-footer>
+  </div>
+</template>
+
+<script>
+  import UserGroup from '../UserGroup'
+  import './assets/scss/vigojug.scss'
+
+  export default {
+    name: 'vigojug',
+    mixins: [UserGroup]
+  }
+</script>
